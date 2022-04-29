216e04b7671b37bb158d1dba2cdcedcdbba16a1c
Reycel Huffman <reycel.huffman@outlook.com>
2022-04-28 22:20:02 -0500

    created a 404 page

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
